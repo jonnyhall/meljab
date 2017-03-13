c11d669964457b8c7eabc9c36f5fd989aa1ccc54
jonnyhall <jonnyhall@hotmail.com>
2017-03-13 13:40:39 -0700

    Microgroove Coding Test

---
+++ CodingTest_csvToJson/Order.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest_csvToJson
+{
+    public class Order
+    {
+        //CSV file has one File record, designated "F" and one End record, designated "E"
+        //Each F record can contain multiple Order records, designated "O".
+        //Each O record contains a single B, S & M & T records
+        //Each O record can contain multiple Line Item records, designated "L"
+        public List<Line> Lines { get; set; }
+
+        public Order(List<Line> lines)
+        {
+            Lines = lines;
+        }
+    }
+
+    public class Column
+    {
+        public string ColumnNumber { get; set; }
+        public string ColumnValue { get; set; }
+    }
+
+    public class Line
+    {
+        public string Name { get; set; }
+        public List<Column> Columns { get; set; }
+        public Line(string name)
+        {
+            Name = name;
+        }
+
+    }
+    public class ELine : Line
+    {
+        public ELine(string name) : base(name)
+        {
+        }
+    }
+    public class LLine : Line
+    {
+        public LLine(string name) : base(name)
+        {
+        }
+    }
+    public class TLine : Line
+    {
+        public TLine(string name) : base(name)
+        {
+        }
+    }
+    public class MLine : Line
+    {
+        public MLine(string name) : base(name)
+        {
+        }
+    }
+
+    public class FLine : Line
+    {
+        public FLine(string name) : base(name)
+        {
+        }
+    }
+    public class OLine : Line
+    {
+        public OLine(string name) : base(name)
+        {
+        }
+    }
+    public class BLine : Line
+    {
+        public BLine(string name) : base(name)
+        {
+        }
+    }
+
+    public class SLine : Line
+    {
+        public SLine(string name) : base(name)
+        {
+        }
+    }
+}
---
+++ CodingTest_csvToJson/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CodingTest_csvToJson
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var csvPath = ConfigurationManager.AppSettings["csvPath"];
+            Console.WriteLine($"csvPath is {csvPath}");
+            var csvFiles = Directory.EnumerateFiles(csvPath, "*.csv", SearchOption.AllDirectories);
+            foreach (var csvFile in csvFiles)
+            {
+                Console.WriteLine($"csvFile name is {csvFile}");
+                var order = ReadCsvFile(csvFile);
+                var jarray = (JArray)JToken.FromObject(order.Lines);
+
+                CreateJsonFile(csvPath, csvFile, jarray);
+            }
+            Console.WriteLine("Finished Creating the JSON file. Type any key to quit...");
+            var exitChar = Console.ReadLine();
+
+        }
+
+        private static Order ReadCsvFile(string fileName)
+        {
+            Console.WriteLine("Reading the CSV file.");
+            var lines = new List<Line>();
+            using (var sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var _line = GetLineByLetter(line.Split(',')[0]);
+                    if (_line == null) throw new ArgumentNullException(nameof(_line));
+                    var columns = new List<Column>();
+                    //there are a total of 23 columns for the longest line. 
+                    //iterating though the columns that have values and adding them to a line
+                    for (var colCount = 1; colCount < 23; colCount++)
+                    {
+                        if (string.IsNullOrEmpty(line.Split(',')[colCount])) continue;
+                        var column = new Column
+                        {
+                            ColumnNumber = colCount.ToString(),
+                            ColumnValue = line.Split(',')[colCount]
+                        };
+                        Console.WriteLine($"Column added to {_line.Name} line. ColumnNumber: {column.ColumnNumber}, ColumnValue: {column.ColumnValue}");
+                        columns.Add(column);
+                    }
+                    _line.Columns = columns;
+                    //adding each line to the list of lines that will be a part of the order 
+                    lines.Add(_line);
+                }
+
+            }
+            var order = new Order(lines);
+
+            return order;
+        }
+
+
+        private static void CreateJsonFile(string directory, string fileName, JArray array)
+        {
+            fileName = Path.Combine(directory, Path.ChangeExtension(fileName, "json"));
+            using (var stream = new StreamWriter(fileName))
+            {
+                using (var writer = new JsonTextWriter(stream))
+                {
+                    writer.Formatting = Formatting.Indented;
+                    array.WriteTo(writer);
+                }
+            }
+        }
+
+
+        private static Line GetLineByLetter(string letter)
+        {
+            switch (letter.ToLower())
+            {
+                case "f":
+                    return new FLine(letter);
+                case "e":
+                    return new ELine(letter);
+                case "o":
+                    return new OLine(letter);
+                case "l":
+                    return new LLine(letter);
+                case "b":
+                    return new BLine(letter);
+                case "s":
+                    return new SLine(letter);
+                case "m":
+                    return new MLine(letter);
+                case "t":
+                    return new TLine(letter);
+            }
+            return null;
+        }
+
+    }
+}
